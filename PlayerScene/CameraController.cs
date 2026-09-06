06ed4d3a83ff7198333a1676fc13c928ba87b9d3
Mike P <mp7252006@gmail.com>
2026-09-06 17:58:34 -0500

    Add code to reference player node in camera controller script

--- PlayerScene/CameraController.cs
+++ PlayerScene/CameraController.cs
@@ -4,17 +4,20 @@ using System;
 public partial class CameraController : Camera3D
 {
 	// Class Variables/Refereces
-	public CharacterBody3D player; // need to figure out how to reference the parent of this node
+	public CharacterBody3D player;
 	private Vector3 offset;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		player = GetNode<CharacterBody3D>(new NodePath(".."));
+		
 		offset = this.Position - player.Position;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
 }
