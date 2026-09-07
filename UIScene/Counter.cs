7b787e8f1b271a39f19f2b99afe7d8d9a9bda48c
Minh <mnguyen131106@gmail.com>
2026-09-06 19:54:14 -0500

    signal works... count increases

--- UIScene/Counter.cs
+++ UIScene/Counter.cs
@@ -6,15 +6,27 @@ public partial class Counter : CanvasLayer
 {
 	// Called when the node enters the scene tree for the first time.
 	private Label counter;
-	const String count = "Count: ";
+	private int count = 0;
 	
+	[Export] public Player player;
 	public override void _Ready()
 	{
 		counter = GetNode<Label>(new NodePath("Counter"));
+		count = 0; // double check to make sure it's 0 at the beginning
+		counter.Text = "Count: " + count; // initial count
+		player.Connect(Player.SignalName.OnHitCollectible, Callable.From(IncreaseCounter));
+	}
+
+	private void IncreaseCounter()
+	{
+		count ++;
+		counter.Text = "Count: " + count;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+
+
 }
