a219cb532a94ba21a0ef7d4d78d8b1f5663595b8
Minh <mnguyen131106@gmail.com>
2026-09-06 20:41:08 -0500

    preliminary enemy AI

---
+++ AIEnemyScene/Enemy.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public partial class Enemy : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	private NavigationAgent3D navAgent;
+
+	[Export] private Player player;
+
+	public override void _Ready()
+	{
+		navAgent = GetNode<NavigationAgent3D>(new NodePath("NavigationAgent3D"));
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		// retarget nav agent
+		navAgent.TargetPosition = player.Position;
+		
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// // Handle Jump.
+		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		// {
+		// 	velocity.Y = JumpVelocity;
+		// }
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector3 direction = (navAgent.GetNextPathPosition() - this.Position).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+
+		MoveAndSlide();
+	}
+}
