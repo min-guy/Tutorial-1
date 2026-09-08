b48147a8d72d899dd8b512b8e8a72ad7a2de1d6b
Mike P <mp7252006@gmail.com>
2026-09-07 20:30:05 -0500

    Add random spawning of collectibles

--- GameScene/GameMap.cs
+++ GameScene/GameMap.cs
@@ -5,10 +5,25 @@ public partial class GameMap : Node3D
 {
 	private DirectionalLight3D globalLight;
 
+	private PackedScene collectibleScene;
+
+	private Timer timer;
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		timer = new Timer();
+		AddChild(timer);
+
+		timer.Timeout += OnTimeout;
+
+		timer.OneShot = false;
+		timer.Start(5);
+
 		globalLight = GetNode<DirectionalLight3D>(new NodePath("DirectionalLight3D"));
+		
+		collectibleScene = GD.Load<PackedScene>("uid://ckhvrjm8fgasq");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,5 +31,14 @@ public partial class GameMap : Node3D
 	{
 		globalLight.Rotate(new Vector3(0, 1, 0), (float)delta * 0.065f);
 		globalLight.Rotate(new Vector3(1, 0, 0), (float)delta * 0.085f);
+	}
+
+	private void OnTimeout()
+	{
+		var collectible = collectibleScene.Instantiate<Collectible>();
+
+		collectible.Position = new Vector3(GD.Randf() * 20.0f - 10, 1.5f, GD.Randf() * 20.0f - 10);
+
+		AddChild(collectible);
 	}
 }
